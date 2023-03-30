400e980a50d3e88e0bbe3ce20ffc60b837e4d5c2
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-30 16:03:26 +0000

    added the coruse

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah Hasan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of Triangle.
+ */
+function calculate () {
+  // input
+  const H = parseInt(document.getElementById('Hight of Trapezoid').value)
+  const A = parseInt(document.getElementById('A of Trapezoid').value)
+  const B = parseInt(document.getElementById('B of Trapezoid').value)
+
+  // process
+  const area = [(A + B) / 2] * H
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' mm² '
+}
